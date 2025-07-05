0467ae4072399084f25283a8f6f03ba08db1a3de
Ashna Singh <ashna.singh2909@gmail.com>
2025-07-05 18:01:21 +0530

    Properly configure Tailwind CSS for production

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,21 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: ['react-fragment-babel-plugin']
+  plugins: [
+    react({
+      babel: {
+        plugins: ['react-fragment-babel-plugin']
+      }
+    })
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer')
+      ]
     }
-  })],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
