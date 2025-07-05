27bf96975ac82e4bec4c4e1d9184e2a02318906e
Ashna Singh <ashna.singh2909@gmail.com>
2025-07-05 17:55:52 +0530

    Update Vite and React configuration

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,26 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: ['react-fragment-babel-plugin']
+    }
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
   }
 })
